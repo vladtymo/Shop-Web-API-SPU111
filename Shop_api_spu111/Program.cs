b9882d9d49792f2d8af89ce48b2948e1a6b83c23
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-11-25 11:22:48 +0200

    AddMigrateOnStartup.cs

--- Shop_api_spu111/Program.cs
+++ Shop_api_spu111/Program.cs
@@ -69,6 +69,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+   var db = scope.ServiceProvider.GetRequiredService<ShopSPUDbContext>();
+   db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
